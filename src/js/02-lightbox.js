8b113ef87633e7e40d589a6a398d5840ff9c32a2
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-07-31 17:17:20 +0200

    02-ligthbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const fotos = galleryItems.map((foto) => 
+`<a class="gallery__item" href="${foto.original}">
+  <img class="gallery__image" 
+    src="${foto.preview}" 
+    alt="${foto.description}"
+    title="${foto.description}" />
+</a>`).join(" ");
+gallery.innerHTML = fotos;
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
+
